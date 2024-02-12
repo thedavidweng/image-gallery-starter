9ea1dc96f7445e6e5b225d84e0001a41fadf499e
Davy <95214375+thedavidweng@users.noreply.github.com>
2024-02-12 01:03:36 -0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,11 +59,10 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
             </div>
             <Logo />
             <h1 className="mt-8 mb-4 text-base font-bold uppercase tracking-widest">
-              2022 Event Photos
+              鲨鲨王国
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
+              一个完全由鲨友组成的开放式鲨图社区！
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
